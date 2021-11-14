422a2906560ecfcf359022b874289ad5196769bf
3imed-jaberi <imed_jebari@hotmail.fr>
2021-11-14 15:56:48 +0100

    beautify the code --test 🧪 ..

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,22 +1,30 @@
-// TODO: need more test cases
 import path from 'path'
 import { expect } from 'chai'
-import { ViewEngine } from '../src'
 
-describe('ViewEngine', function () {
-  it('should render page ok', async () => {
-    const engine = new ViewEngine({
+import ViewEngine from '../src'
+
+describe('ViewEngine', () => {
+  it('should throw when pass invalid settings object', () => {
+    expect(
+      () =>
+        new ViewEngine({
+          root: path.join(__dirname, 'views'),
+          x: 'hello',
+          viewExt: 'html',
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } as any)
+    ).to.Throw
+  })
+
+  it('should return the complied string from the template', async () => {
+    const viewEngine = new ViewEngine({
       root: path.join(__dirname, 'views'),
-      layout: 'template',
       viewExt: 'html',
+      cache: true
     })
 
-    const users = [
-      { name: 'Jawher' },
-      { name: 'Imed' },
-      { name: 'Tom' },
-    ]
-    const html = await engine.tempRender('home', { users })
+    const users = [{ name: 'Jawher' }, { name: 'Imed' }, { name: 'Tom' }]
+    const html = await viewEngine.generateHtml('home', { users })
 
     expect(html.startsWith('<ul>')).to.be.true
     expect(html.includes('<li>Jawher</li>')).to.be.true
